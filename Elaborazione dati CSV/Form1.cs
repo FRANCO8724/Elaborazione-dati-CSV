2f90be54934b290c8f3f58fad8eb81cc1e4b0069
FRANCOHAILIMITIDELLEVOLUTO <121171151+FRANCO8724@users.noreply.github.com>
2023-05-29 22:47:55 +0200

    Lunghezza max record e campi

--- Elaborazione dati CSV/Form1.cs
+++ Elaborazione dati CSV/Form1.cs
@@ -16,6 +16,7 @@ namespace Elaborazione_dati_CSV
     public partial class Form1 : Form
     {
         public string path = @"../../Arrigoni.csv";
+        public int lung=0;
 
         public Form1()
         {
@@ -43,7 +44,7 @@ namespace Elaborazione_dati_CSV
         }
         private void button3_Click_1(object sender, EventArgs e)
         {
-
+            lung = Lungmax();
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -133,6 +134,75 @@ namespace Elaborazione_dati_CSV
                 listView1.Clear();
                 listView1.Items.Add("Ogni record è composto da: " + lun + " campi.");
             }
+        }
+
+        public int Lungmax()
+        {
+            int lung = 0;
+
+            using (StreamReader sw = new StreamReader(path))
+            {
+                int dim = 0;
+
+                string a = sw.ReadLine();
+
+                string[] campi = a.Split(';');
+
+                int[] arr = new int[(campi.Length) + 1];
+
+                for (int i = 0; i < campi.Length; i++)
+                {
+                    arr[dim] = campi[i].Length;
+                    dim++;
+                }
+                arr[(arr.Length) - 1] = a.Length;
+
+                while (a != null)
+                {
+                    dim = 0;
+
+                    string[] campi2 = a.Split(';');
+
+
+                    for (int i = 0; i < campi2.Length; i++)
+                    {
+                        if (arr[dim] < campi2[i].Length)
+                        {
+                            arr[dim] = campi2[i].Length;
+                        }
+
+                        dim++;
+                    }
+
+                    if (arr[(arr.Length) - 1] < a.Length)
+                    {
+                        arr[(arr.Length) - 1] = a.Length;
+                    }
+
+                    a = sw.ReadLine();
+
+                }
+
+                dim = 0;
+
+                listView1.Clear();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (i != arr.Length - 1)
+                    {
+                        listView1.Items.Add("Lunghezza campo " + dim.ToString() + ": " + arr[i]);
+                    }
+                    else
+                    {
+                        listView1.Items.Add("Lunghezza record " + dim.ToString() + ": " + (arr[arr.Length - 1] + 1));
+                    }
+                    dim++;
+                }
+
+                lung = arr[arr.Length - 1] + 1;
+            }
+
+            return lung;
         }
     }
 }
